24dfb3943f13b2785ee256457d72fd5825db79ef
Vessa-is <islami.vessa@gmail.com>
2026-04-21 06:25:43 +0200

    Implement manual processing
    Simulon sistemin tradicional ku përpunimi i pacientëve është më i ngadalshëm për shkak të proceseve manuale.

--- spitali.cpp
+++ spitali.cpp
@@ -8,3 +8,11 @@ queue<string> patients;
 patients.push("Patient A");
 patients.push("Patient B");
 patients.push("Patient C");
+
+void processManual(queue<string> q) {
+    while (!q.empty()) {
+        cout << "Processing: " << q.front() << endl;
+        q.pop();
+        this_thread::sleep_for(chrono::milliseconds(800));
+    }
+}
